96dbc9770cc43cbceb5f9730904da4652c4bba3c
JoaoMateus <joaomateusabm@yahoo.com.br>
2020-08-04 23:21:19 -0300

    Primeiro dia da NLW

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css';
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars2.githubusercontent.com/u/54962256?s=460&v=4" alt="João Mateus" />
+                <div>
+                    <strong>João Mateus</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br /><br />
+                Apaixonado por esplodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                        Entrar em contato
+                    </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;

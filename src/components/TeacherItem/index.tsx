a10d6dd74935da66f7dbc24b7464f4aaca58e976
JoaoMateus <joaomateusabm@yahoo.com.br>
2020-08-06 20:36:08 -0300

    Terceiro dia da NLW Front-end integrado a api criada na aula passada.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,34 +2,51 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher  {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;      
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
+    function createNewConncetion(){
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/54962256?s=460&v=4" alt="João Mateus" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>João Mateus</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por esplodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConncetion}href={`https://wa.me/${teacher.whatsapp}`} target="blank">
                     <img src={whatsappIcon} alt="Whatsapp" />
                         Entrar em contato
-                    </button>
+                    </a>
             </footer>
         </article>
     )
